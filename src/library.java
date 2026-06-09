44548f0a0922fa3a5380feef5dd7c1af903878cd
iamutkarshkatiyar <utkarshkatiyar2004@gmail.com>
2026-06-09 23:58:07 +0530

    library class added

---
+++ src/library.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class library {
+    ArrayList<book> Books ;
+    library(){
+        Books =new ArrayList<>();
+    }
+    void addBook(book Book){
+        Books.add(book);
+
+    }
+}
